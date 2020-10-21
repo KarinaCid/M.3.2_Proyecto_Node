3091e06bad73af7d3f887217ab89e4c11beb0b96
Karina Cid <memento_mori.06@hotmail.com>
2020-10-21 01:05:30 -0300

    Update repository
    Project finished. In current commit, update the work components from origin

--- controllers/users.js
+++ controllers/users.js
@@ -13,3 +13,14 @@ exports.user_create = function(req, res, next) {
     res.json({status: 'ERROR', message: 'Debe completar todos los campos'}); //opcional mandar un mensaje de error
   }
 }
+
+exports.user_get = async function(req, res, next) {
+  var users = await User.find({})
+  var listUsers = users.map((user) => {
+    user.lastname = user.lastname.replace("ñ","nn");
+    // Replace , para cambiar "ñ" por "nn"
+  return user;
+});
+return listUsers.sort();
+// sort que ordena de forma alfabéticamente 
+};
